6abfe4062b6cab7784f3051ba1eaa1c766faba4c
yasirkula <yasirkula@yahoo.com>
2021-05-18 13:36:23 +0300

    Unity 2020 support

--- Plugins/RuntimeTexture/Editor/RuntimeTextureEditor.cs
+++ Plugins/RuntimeTexture/Editor/RuntimeTextureEditor.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEditor;
+#if UNITY_2020_2_OR_NEWER
+using UnityEditor.AssetImporters;
+#else
 using UnityEditor.Experimental.AssetImporters;
+#endif
 using UnityEngine;
 
 namespace RuntimeTextureNamespace
--- Plugins/RuntimeTexture/Editor/RuntimeTextureImporter.cs
+++ Plugins/RuntimeTexture/Editor/RuntimeTextureImporter.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
+#if UNITY_2020_2_OR_NEWER
+using UnityEditor.AssetImporters;
+#else
 using UnityEditor.Experimental.AssetImporters;
+#endif
 using System.IO;
 using UnityEditor;
 using System.Collections.Generic;
